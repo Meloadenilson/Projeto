e92bd2ac2c319b02ea0b8c46cb5332beb11dd7f6
Meloadenilson <meloadenilson.sbv@gmail.com>
2018-03-21 11:25:15 -0300

    primeiro commit

---
+++ Assets/Scripts/VidaPersonagem.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class VidaPersonagem : MonoBehaviour {
+	float vida;
+	public Text vidaTexto;
+
+	// Use this for initialization
+	void Start () {
+		vida = 100;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		vidaTexto.text = vida.ToString("Vida 0");
+
+		if (vida == 0){
+			SceneManager.LoadScene ("gameOver");
+		}
+	}
+
+
+	void OnCollisionEnter2D(Collision2D colisao){
+			if (colisao.gameObject.tag == "dano") 
+			{
+				vida -= 10; 
+			}
+			
+		}
+
+}
